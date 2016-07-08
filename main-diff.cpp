9ae5b8491cd632d1ff8d5206ef3ba674fe8563a8
Lukas W <lukaswhl@gmail.com>
2016-07-08 22:55:52 +1200

    Implement diff utility

--- main-diff.cpp
+++ main-diff.cpp
@@ -2,12 +2,23 @@
 #include "sqlite3.h"
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void trace_callback(void* udp, const char* sql) {
 	std::cerr << "{SQL} " << sql << std::endl;
 }
+
+#define SQLOK(X) do {														\
+	rc = X; 																\
+	if (rc != 0) {															\
+		std::cerr << "Error: " << #X << " returned " << rc << std::endl;	\
+		std::cerr << sqlite3_errstr(rc) << std::endl;						\
+		return rc;															\
+	}																		\
+} while(0)
+
 
 int main(int argc, char const *argv[])
 {
@@ -23,15 +34,20 @@ int main(int argc, char const *argv[])
 	sqlite3* db;
 	rc = sqlite3_open_v2(db1File, &db, SQLITE_OPEN_READWRITE, nullptr);
 
-	sqlite3_trace(db, trace_callback, NULL);
+	//sqlite3_trace(db, trace_callback, NULL);
 
 	if (rc != SQLITE_OK) {
 		cerr << "Could not open sqlite DB " << db1File << endl;
 		return 2;
 	}
 
-	//TODO: Attach
-	//TODO: Call diff
+	SQLOK(sqlite3_exec(db, (string() + "ATTACH '" + db2File + "' AS 'aux';").data(), 0, 0, 0));
+
+	sqlitediff_diff_prepared(
+		db,
+		nullptr,
+		stdout
+	);
 
 	if (rc != SQLITE_OK) {
 		cerr << "Could not create changeset." << endl;
